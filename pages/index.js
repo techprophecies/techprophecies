3d45e40f5f92518648b23d7b3437759df5198678
moisestech <m@moises.tech>
2022-01-25 22:35:03 -0500

    Banner comp

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
 import Background from '../components/Background';
 import Feature from '../components/Feature';
 import Grid from '../components/Grid';
+import Footer from '../components/Footer';
+import Banner from '../components/Banner';
 
 export default function IndexPage() {
   return (
     <>
+      <Banner />
       <Background />
       <Feature />
       <Grid />
+      <Footer />
     </>
   );
 }
