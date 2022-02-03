472d371d0c5d35ab5a717ce0402e481cf9d97bd1
moisestech <m@moises.tech>
2022-02-03 17:19:44 -0500

    Banner, Feature, Grid, Footer Layout in Index

---
+++ components/index.js
@@ -0,0 +1,31 @@
+// NAV
+export {default as Header} from './Header';
+export {default as Logo} from './Logo';
+export {default as Menu} from './Menu';
+export {default as Search} from './Search';
+
+// LAYOUT
+export {default as GridHeader} from './GridHeader';
+export {default as Grid} from './Grid';
+export {default as Page} from './Page';
+
+// FOOTER
+export {default as Footer} from './Footer';
+
+// CARD
+export {default as Card} from './Card';
+export {default as Feature} from './Feature';
+export {default as Background} from './Background';
+
+// VIDEO
+export {default as Video} from './Video';
+
+// FRONT PAGE
+export {default as Banner} from './Banner';
+export {default as Symbol} from './Symbol';
+
+// WEB3
+export {default as Wallet} from './Wallet';
+
+// EFFECTS
+export {default as LoadingGlow} from './LoadingGlow';
